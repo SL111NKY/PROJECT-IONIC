6a9fd1d439e7071f44fb5866f322cc7e96173750
abc <a@b.c>
2024-03-31 08:50:55 +0200

    added product card component and did some tests

--- src/app/sharedmodule/sharedmodule.module.ts
+++ src/app/sharedmodule/sharedmodule.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { InfoBoxComponent } from './info-box/info-box.component';
 import { IonicModule } from '@ionic/angular';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProductcardComponent } from './productcard/productcard.component';
 
 
 
 @NgModule({
-  declarations: [InfoBoxComponent, NavbarComponent],
+  declarations: [InfoBoxComponent, NavbarComponent, ProductcardComponent],
   imports: [
     CommonModule, IonicModule
   ],
-  exports: [InfoBoxComponent, NavbarComponent],
+  exports: [InfoBoxComponent, NavbarComponent, ProductcardComponent],
 })
 export class SharedmoduleModule { }
