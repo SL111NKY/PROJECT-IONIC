ed4dcefa7a9e34bdcdd226dd18850c786267b620
SL111NKY <a@b.c>
2024-04-07 06:25:57 +0200

    added navbar to shared module and single pages

--- src/app/sharedmodule/sharedmodule.module.ts
+++ src/app/sharedmodule/sharedmodule.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
+import { RouterModule, Routes } from '@angular/router';
 
 
 @NgModule({
-  declarations: [FooterComponent],
+  declarations: [FooterComponent, NavbarComponent],
   imports: [
-    CommonModule, IonicModule
+    CommonModule, IonicModule, RouterModule
   ],
-  exports: [FooterComponent],
+  exports: [FooterComponent, NavbarComponent, RouterModule],
 })
 export class SharedmoduleModule { }
