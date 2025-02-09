e3535c4ebfec8bb2d8a369b48a217325c83f1b58
SL111NKY <vngeloskirkos@gmail.com>
2025-02-09 01:12:58 +0100

    added google auth and flowz api call

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,9 +1,20 @@
 import { AuthService } from '../services/auth.service';
 import { Component, inject, Inject, INJECTOR, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 
-
+interface CANNA {
+  id: string;
+  category: string;
+  country: string;
+  description:string;
+  firma:string;
+  genetik:string;
+  kultivar:string;
+  sorte:string;
+  thc:number;
+}
 
 @Component({
   selector: 'app-login',
@@ -16,8 +27,10 @@ export class LoginPage implements OnInit {
 
   user: any = null;
   clientId = '545762027595-83hnm2u56k7qoq04mg051jfdbm9n1sps.apps.googleusercontent.com'; // Ersetze mit deiner Client-ID
+  data: CANNA[] = [];
 
-  constructor(private authService: AuthService) {}
+ 
+  constructor(private authService: AuthService, private dataService: DataService) {}
 
   ngOnInit() {
     this.authService.user$.subscribe(user => {
@@ -25,6 +38,27 @@ export class LoginPage implements OnInit {
     });
 
     this.authService.initializeGoogleSignIn(this.clientId);
+
+
+
+
+    this.dataService.fetchData().subscribe(
+      (response: any) => {
+        this.data = response;
+        console.log('Fetched Data:', this.data);
+        const firstPost = this.data[0];
+        console.log('First Post Title:', firstPost.id);
+        console.log('First Post Body:', firstPost.category);
+
+      },
+      (error: any) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+
+
+
+
   }
 
   logout() {
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface CANNA {
+  id: string;
+  category: string;
+  country: string;
+  description:string;
+  firma:string;
+  genetik:string;
+  kultivar:string;
+  sorte:string;
+  thc:number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private apiUrl = 'https://europe-west3-au-digital.cloudfunctions.net/dransay/api/webshop/products?sandbox=0'; // Dein Ziel-API-URL
+
+  constructor(private http: HttpClient) { }
+
+  fetchData(): Observable<CANNA> {
+    // Definiere die Header für die Anfrage
+    const headers = new HttpHeaders({
+      'accept': 'application/json, text/plain, */*',
+      'accept-language': 'de-DE,de;q=0.9,nl-NL;q=0.8,nl;q=0.7,en-US;q=0.6,en;q=0.5',
+      'priority': 'u=1, i',
+      'sec-ch-ua': '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"Windows"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'cross-site',
+      'x-api-key': '3e36683d451d9bbb1168ce65dcd17c775a701346788f3d4b76eb4e9de05463c1',
+      'Referrer-Policy': 'strict-origin-when-cross-origin'
+    });
+
+    // Führe den GET-Request mit den Headern aus und gebe das Observable zurück
+    return this.http.get<CANNA>(this.apiUrl, { headers });
+  }
+}
